761286a8023126666731ef2ddbd438659a3066da
constantcode9909 <mohamedaminebofficiel@gmail.com>
2023-06-13 15:33:52 +0200

    final JS reformed version

--- Background Color Flipper/backgroundColorFlipperJsCode.js
+++ Background Color Flipper/backgroundColorFlipperJsCode.js
@@ -1,12 +1,14 @@
+// get the original value of the background color property of the main element
 const mainElement = document.querySelector("main");
 const mainElementComputedStyles = getComputedStyle(mainElement);
 const backgroundColorValue = mainElementComputedStyles.backgroundColor;
 const targetArticle = document.querySelector("article");
 const originalText = targetArticle.innerText;
 
+
 const targetButton = document.getElementById("target_button");
 const randomColorValues = ["Green", "#F15025", "Red", "Rgba(133,122,200)"];
-const hexColorValues = ["#b0bdb336", "#BF1421", "#B6BCB2", "#E3F851", "#BA1F83", "#83C864", "#56FDC0", "#8C05BC", "#0E3121", "#6F82C3", "#F5ACC4"];
+const hexColorValues = ["A", "B", "C", "D", "E", "F", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 
 const simpleButton = document.getElementById("Simple_button");
@@ -24,33 +26,50 @@ function rgbaToHex(rgbaColor) {
   
     return "#" + hexComponents.join("");
   }
-targetArticle.innerText = originalText + rgbaToHex(backgroundColorValue);
 
+// reset the background color value to the original value
+// set it in the article inner text
+function resetBackgroundColor(){
+    mainElement.style.backgroundColor = backgroundColorValue;
+    targetArticle.innerText = originalText + rgbaToHex(backgroundColorValue);
+}
 
+resetBackgroundColor();
 
+// get a random number and use it to get a random value from the array provided
 function colorRandomizer(listOfColors){
          var result = Math.floor(Math.random() * listOfColors.length );
          return listOfColors[result];
 }
 
+// generate hex colors
+function hexColorGenerator(){
+    let listOfHexComponents = ["#"];
+    let index = 0;
+    while(index < 6){
+        listOfHexComponents.push(colorRandomizer(hexColorValues));
+        index++;
+    }
+    return listOfHexComponents.join("");
+}
+
+// manage the event listener and which functions it uses to generate color values
+function eventManager (eventAction , valueForTheSimpleButton){
 targetButton.addEventListener("click" , function(){
-    let newRandomColor = colorRandomizer(randomColorValues);
+    let newRandomColor = eventAction(valueForTheSimpleButton);
     mainElement.style.backgroundColor = newRandomColor;
     targetArticle.innerText = originalText + newRandomColor;
 })
+}
 
 
 simpleButton.addEventListener("click" , function () {
-    mainElement.style.backgroundColor = backgroundColorValue;
-    targetArticle.innerText = originalText + rgbaToHex(backgroundColorValue);
+    resetBackgroundColor();
+    eventManager(colorRandomizer , randomColorValues);
 })  
 
 
 hexButton.addEventListener("click", function (){
-    mainElement.style.backgroundColor = backgroundColorValue;
-    targetButton.addEventListener("click" , function(){
-        let newRandomColor = colorRandomizer(hexColorValues);
-        mainElement.style.backgroundColor = newRandomColor;
-        targetArticle.innerText = originalText + newRandomColor;
-    })
+    resetBackgroundColor();
+    eventManager(hexColorGenerator)
 })
