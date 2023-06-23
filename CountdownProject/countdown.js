f0ad10a44dca0fcf45451970d635b177a93f76f3
constantcode9909 <mohamedaminebofficiel@gmail.com>
2023-06-23 17:25:32 +0200

    second changes

--- CountdownProject/countdown.js
+++ CountdownProject/countdown.js
@@ -1,47 +1,60 @@
 "use strict";
-const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-// let date = new Date();
 
-function updateDateBy10Days(){
-    let date = new Date(2023 , 6 , 2 , 11 , 30);
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+const deadlineTime = setGiveawayDate().getTime();
+const days = document.querySelector(".Days");
+const hours = document.querySelector(".Hours");
+const mins = document.querySelector(".Mins");
+const secs = document.querySelector(".Secs");
+const currentDate = new Date().getTime();
+
+
+// set the giveawy date
+function setGiveawayDate(){
+    // using the spread operator
+    const args = [2023, 6, 2, 11, 30];
+    const date = new Date(...args);
     const currentDay = date.getDate();
-    console.log(date);
     date.setDate(currentDay);
     return date;
 }
+
 
 function getMonthString(date1){
     const monthIndex = date1.getMonth();
     return month[monthIndex];
 }
 
+
+// use the defined giveaway date and change the content of the page
 function updateGiveawayDate(){
     const targetElement = document.getElementById("giveaway_time");
-    const giveawyaDate = updateDateBy10Days();
-    let targetElementListVersion = targetElement.innerText.split(" ");
-    targetElementListVersion[0] = giveawyaDate.getDate();
-    targetElementListVersion[1] = getMonthString(giveawyaDate);
-    targetElementListVersion[2] = giveawyaDate.getFullYear();
-    const newDate = targetElementListVersion.join(' ');
+    const giveawyaDate = setGiveawayDate();
+    let dateList = targetElement.innerText.split(" ");
+    dateList[0] = giveawyaDate.getDate();
+    dateList[1] = getMonthString(giveawyaDate);
+    dateList[2] = giveawyaDate.getFullYear();
+    const newDate = dateList.join(' ');
     targetElement.innerText = newDate;
 }
 
 updateGiveawayDate();
 
 
-const deadlineTime = updateDateBy10Days().getTime();
-const days = document.querySelector(".Days");
-const hours = document.querySelector(".Hours");
-const mins = document.querySelector(".Mins");
-const secs = document.querySelector(".Secs");
+// an implementation of a countdown timer using the a function that will be passed to setInterval
+// the date must be redefined every second to ensure the updated values are calculated properly
+function updateCountDownTimer(){
+    const now = new Date();
+    const currentTime = now.getTime();
+    let timeDifference = deadlineTime - currentTime;
+    days.innerText = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    hours.innerText = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    mins.innerText = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    secs.innerText = Math.floor((timeDifference % (1000 * 60)) / 1000);
+}
 
 
-setInterval(()=>{
-    const date2 = new Date();
-    const currentTime = date2.getTime();
-    let distance = deadlineTime - currentTime;
-    days.innerText = Math.floor(distance / (1000 * 60 * 60 * 24));
-    hours.innerText = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    mins.innerText = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    secs.innerText = Math.floor((distance % (1000 * 60)) / 1000);
-},1000)
+
+updateCountDownTimer();
+
+setInterval(updateCountDownTimer , 1000);
