c2f99aae76fef90060dc11a4eb564dca023af2e9
constantcode9909 <mohamedaminebofficiel@gmail.com>
2023-06-24 17:12:19 +0200

    second commit

---
+++ Scroll project/scroll.js
@@ -0,0 +1,9 @@
+const revealLinks = document.querySelectorAll(".reveal_link");
+const headerSection = document.querySelector("nav");
+
+revealLinks.forEach((e)=>{
+    e.addEventListener("click" , function(event){
+        event.preventDefault();
+        headerSection.classList.toggle("reveal_menu");
+    })
+})
