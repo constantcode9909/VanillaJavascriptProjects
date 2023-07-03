b88bff58952743854c1bcf9575fab312b25371ff
constantcode9909 <mohamedaminebofficiel@gmail.com>
2023-07-04 00:47:16 +0200

    finished project

--- Video Project/video.js
+++ Video Project/video.js
@@ -2,10 +2,19 @@
 
 const targetVideo = document.querySelector("video");
 const controlBtns = document.querySelectorAll("button");
+const coverItem = document.querySelector(".cover-box");
+const preloaderContainer = document.querySelector('header');
+
+
+window.addEventListener("load", ()=>{
+    preloaderContainer.style.display = "none";
+})
+
 
 controlBtns.forEach(function(btn){
     btn.addEventListener("click",()=>{
         const targetPlayBtn = btn.classList.contains("play");
-        targetPlayBtn ? targetVideo.play(): targetVideo.pause();
+        targetPlayBtn ? targetVideo.play():targetVideo.pause();
+        coverItem.classList.toggle("move-box");
     })
 })
